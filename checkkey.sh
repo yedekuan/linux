0515f1d08d73fb4945d188da5d3da2b5901b3f1b
yedekuan <yedekuan@163.com>
2017-02-22 11:10:24 +0800

    sucess ch

--- checkkey.sh
+++ checkkey.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
-read -p "please input an word" key
+read -p "please input an word: " key
 case "$key" in
 [a-z][A-Z])
   echo "It is letter"
 ;;
 [0-9])
   echo "It is figure"
+;;
 *)
   echo "others"
 esac
